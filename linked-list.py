fce7f59f30415c45a1ace1a7fc46349872fbe201
Sarthak Manchanda <sarthakmanchanda1011@gmail.com>
2018-11-05 22:44:42 +0000

    Update linked-list.py

--- linked-list.py
+++ linked-list.py
@@ -10,12 +10,38 @@ class LinkedList:
 	def __init__(self):
 		self.head = None
 
-	def prinntList(self):
+	def printList(self):
 		temp = self.head
 		while (temp):
 			print temp.data,
 			temp = temp.next
 
+	def insert_node(self, prev_node, new_data):
+		# at any given position 
+		new_node = Node(new_data)
+		new_node.next = prev_node.next
+		prev_node.next = new_node	
+		
+
+	def push(self, new_data):
+		# at the beginning
+		new_node = Node(new_data)
+		new_node.next = self.head
+		self.head = new_node
+
+
+	def append(self, new_data):
+		# at the end 
+		new_node = Node(new_data)
+		if self.head is None:
+			self.head = new_node
+			return
+
+		last = self.head
+		while(last.next):
+			last = last.next
+
+		last.next = new_node
 
 
 if __name__ == '__main__':
@@ -27,4 +53,4 @@ if __name__ == '__main__':
 	llist.head.next = second
 	second.next = third
 
-	llist.prinntList()
+	llist.printList()
